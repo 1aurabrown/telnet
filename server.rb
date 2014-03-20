159b25e47cf27d6f0e09fcd956882d342987d067
Laura Brown <laurabrown1113@gmail.com>
2014-03-20 14:56:02 -0400

    able to get an xapp token

--- server.rb
+++ server.rb
@@ -2,24 +2,26 @@ require 'net/http'
 require 'eventmachine'
 require 'yaml'
 require 'uri'
+require 'pry'
+require 'json'
 
-class ArtsyServer < EM::Connection
-
+module ArtsyServer
   def client_config
-    @@client_config ||= client_config = YAML.load_file(Rails.root.join("/config.yml"))
+    client_config ||= client_config = YAML.load_file("config.yml")
   end
 
   def get_xapp_token
-    url = URI.parse("https://artsyapi.com/api/v1/xapp_token?client_id=#{client_config['CLIENT_ID']}&client_secret=#{client_config['CLIENT_SECRET']}")
-    req = Net::HTTP::Get.new(url.path)
-    res = Net::HTTP.start(url.host, url.port) {|http|
-      http.request(req)
+    uri = URI("https://artsyapi.com/api/v1/xapp_token?client_id=#{client_config['CLIENT_ID']}&client_secret=#{client_config['CLIENT_SECRET']}")
+
+    Net::HTTP.start(uri.host, uri.port, use_ssl: true) {|http|
+      req = Net::HTTP::Get.new uri
+      response = http.request req
+      return JSON.parse(response.body)["xapp_token"]
     }
-    return res.body
   end
 
   def xapp_token
-    @@xapp_token ||= get_xapp_token
+    xapp_token ||= get_xapp_token
   end
 
   def receive_data(data)
@@ -31,8 +33,8 @@ class ArtsyServer < EM::Connection
   end
 
   def post_init
-    puts xapp_token
     send_artsy_logo
+    puts xapp_token
   end
 
   def send_artsy_logo
@@ -41,15 +43,14 @@ class ArtsyServer < EM::Connection
     file.each {|line|
       contents << line
     }
-    puts contents
     send_data(contents)
   end
+end
 
-  EventMachine.run do
-    # hit Control + C to stop
-    Signal.trap("INT")  { EventMachine.stop }
-    Signal.trap("TERM") { EventMachine.stop }
+EventMachine.run do
+  # hit Control + C to stop
+  Signal.trap("INT")  { EventMachine.stop }
+  Signal.trap("TERM") { EventMachine.stop }
 
-    EventMachine.start_server("0.0.0.0", 1234, ArtsyServer)
-  end
+  EventMachine.start_server("0.0.0.0", 1234, ArtsyServer)
 end
